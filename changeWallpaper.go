b64fdf12a636ea1507fcf3407519d0305512fd27
cpthgli <cpthgli@gmail.com>
2017-03-06 19:20:46 +0900

    Add feature: Write the log to a file. Add a -log option, it also writes the log to a file.

--- changeWallpaper.go
+++ changeWallpaper.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"flag"
+	"io"
 	"io/ioutil"
 	"log"
 	"math/rand"
+	"os"
 	"os/exec"
 	"path/filepath"
 	"strings"
@@ -22,7 +24,7 @@ func hasSuffixes(s string, suffixes []string) bool {
 func getImageFilePaths(root string) []string {
 	fileInfos, err := ioutil.ReadDir(root)
 	if err != nil {
-		log.Fatalf("Error: %v", err)
+		log.Fatalf("ERROR %v", err)
 	}
 	paths := []string{}
 	suffixes := []string{".jpg", ".jpeg", ".png"}
@@ -35,11 +37,21 @@ func getImageFilePaths(root string) []string {
 	return paths
 }
 func main() {
+	isLoging := flag.Bool("log", false, "write log for bool")
 	flag.Parse()
+	if *isLoging {
+		logfile, err := os.OpenFile("./debug.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+		if err != nil {
+			panic("cannot open debug.log:" + err.Error())
+		}
+		defer logfile.Close()
+		log.SetOutput(io.MultiWriter(logfile, os.Stdout))
+	}
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
 	root, err := filepath.Abs(flag.Arg(0))
-	log.Printf("Specified root path is %v", root)
+	log.Printf("INFO Specified root path is %v", root)
 	if err != nil {
-		log.Fatalf("Error: %v", err)
+		log.Fatalf("ERROR %v", err)
 	}
 	paths := getImageFilePaths(root)
 	path := ""
@@ -55,8 +67,8 @@ func main() {
 	)
 	out, err := command.Output()
 	if err != nil {
-		log.Fatalf("Error: %v", err)
+		log.Fatalf("ERROR %v", err)
 	}
-	log.Printf("Command output %v", out)
-	log.Printf("Set wallpaper %v", path)
+	log.Printf("INFO Command output %v", out)
+	log.Printf("INFO Set wallpaper %v", path)
 }
